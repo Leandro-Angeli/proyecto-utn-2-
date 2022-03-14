4d7b3b91eb1826e96e9062c1062e3992dfaa41b6
leandro angeli <kurco91@hotmail.com>
2022-03-14 13:42:30 -0300

    conditional log  env vars

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { environment as prodVars } from 'src/environments/environment.prod';
-console.log('let from environment', environment);
-console.log('let from development', prodVars);
+environment ? console.log('let from environment', environment) : null;
+prodVars ? console.log('let from development', prodVars) : null;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
