7fdaf654c4fe9cd3a8e03852e849a78ff3253e6a
leandro angeli <kurco91@hotmail.com>
2022-03-14 15:14:27 -0300

    display env var

--- src/app/components/home-header/home-header.component.ts
+++ src/app/components/home-header/home-header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
-  styleUrls: ['./home-header.component.scss']
+  styleUrls: ['./home-header.component.scss'],
 })
 export class HomeHeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  env: string = '';
+  constructor() {
+    this.env = environment.test;
   }
 
+  ngOnInit(): void {}
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
-import { environment as prodVars } from 'src/environments/environment.prod';
-environment ? console.log('let from environment', environment) : null;
-prodVars ? console.log('let from development', prodVars) : null;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
