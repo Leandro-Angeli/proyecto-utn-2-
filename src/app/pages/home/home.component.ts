774f87f6c363c3d201d7fdfe5bb56f139a069c94
leandro angeli <kurco91@hotmail.com>
2022-03-14 12:57:21 -0300

    env variables

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
+import { environment as prodVars } from 'src/environments/environment.prod';
+console.log('let from environment', environment);
+console.log('let from development', prodVars);
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
