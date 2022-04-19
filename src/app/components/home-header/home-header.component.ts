aa63b25ed876be76e148fee83d3d13a44552ab6d
Leandro Angeli <leandroandresangeli@gmail.com>
2022-04-18 23:11:39 -0300

    deleted env

--- src/app/components/home-header/home-header.component.ts
+++ src/app/components/home-header/home-header.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
   styleUrls: ['./home-header.component.scss'],
 })
 export class HomeHeaderComponent implements OnInit {
-  env: string = '';
-  constructor() {
-    this.env = environment.test;
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
